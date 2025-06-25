c7390c861e39323e866d9b7a4761b92ef8874855
VyaS-009 <vedavyas1014@gmail.com>
2025-06-25 21:32:14 +0530

    chore: update dependencies and configuration
    - Updated @types/node from 24.0.3 to 24.0.4 in package.json.
    - Reorganized imports in App.tsx for consistency.
    - Removed unused Tailwind CSS directives in index.css and added theme variables.
    - Updated tsconfig.app.json and tsconfig.json to improve TypeScript configuration.
    - Added Tailwind CSS plugin to Vite configuration.
    - Created package-lock.json for dependency management.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
 // src/App.tsx
-import Login  from "./pages/login/login";
-import {  BrowserRouter as Router, Routes, Route } from  "react-router";
-import SignUp from "./pages/sign-up/sign-up"
-import { Button } from "@/components/ui/button";
+import Login from "./pages/login/login";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import SignUp from "./pages/sign-up/sign-up";
+// import { Button } from "@/components/ui/button";
 import Dashboard from "@/pages/dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element ={  <Login />}/>
-<Route path="/sign-up" element ={  <SignUp  />}/>
+        <Route path="/" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
